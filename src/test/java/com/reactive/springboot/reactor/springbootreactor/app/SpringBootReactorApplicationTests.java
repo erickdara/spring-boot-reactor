521b0c21d2b5e3d87a7ac364d085a0e0146f7abd
Erick David Rangel Garcia <erick1rg@gmail.com>
2022-08-13 00:04:49 -0500

    Initial Commit

---
+++ src/test/java/com/reactive/springboot/reactor/springbootreactor/app/SpringBootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.springboot.reactor.springbootreactor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
